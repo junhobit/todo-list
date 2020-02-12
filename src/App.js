a7706435331d34da0958769a86761134dcfb9f23
junhobit <krs199228@gmail.com>
2020-02-12 19:50:15 +0900

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
 
 class App extends Component{
   render(){
     return (
-      <div>App</div>
+      <TodoListTemplate form={<Form/>}>
+        템플릿 완성
+      </TodoListTemplate>
     );
   }
 }
