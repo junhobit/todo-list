5f9e20d4edda28eb1a8c89976143213a65b02dad
junhobit <krs199228@gmail.com>
2020-02-23 07:45:29 +0900

    practice

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
@@ -6,9 +7,11 @@ class TodoItemList extends Component {
     
         return (
             <div>
-                
+                <TodoItem text="테스트1"/>
+                <TodoItem text="테스트2"/>
+                <TodoItem text="테스트3"/>
             </div>
         );
     }
-
 }
+export default TodoItemList;
